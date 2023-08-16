e62fe91c671e8e849b5d6c7ab2e6c33133a1450a
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-08-17 02:46:02 +0530

    hero section complete - making it so responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>
+      <body className={`${inter.className}`} >
         <NavBar/>
           {children}
         <Footer/>
--- src/components/hero/HeroSectionMain.jsx
+++ src/components/hero/HeroSectionMain.jsx
@@ -1,10 +1,50 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import styles from "./css/hero.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
+import classmateImage from "../../../public/images/classmates.jpg"
 const HeroSectionMain = () => {
   return (
     <>
        <div className={styles.hero_main_container} >
-          <h1>Hello I am hero section</h1>
+          <div className={styles.container_box} >
+          {/* left_section */}
+             <div className={`${styles.left_section} ${styles.hero_main_items}`} >
+                  <h1 className={styles.hero_heading} >
+                      Welcome to IGIT <br />
+                      MCA student <br />
+                      website.
+                   </h1>
+                   <p className={styles.hero_text} >
+                   This page is developed by MCA 41th in view of helping the juniors.This page will help to connect their seniors and juniors.It includes. all the semester notes, questions and assignments. It lets you to contact your senior throught their instagram and linkedin profile.
+                   </p>
+
+                   <div>
+                       <Link className={styles.hero_btn} href={"/login"} >
+                          Get started
+                       </Link>
+                   </div>
+            </div>
+             {/* left_section ends*/}
+             
+             {/* right_section start*/}
+             <div className={`${styles.right_section} ${styles.hero_main_items}`} >
+                 {/* carosule image */}
+                 <div className={styles.carosule_img_box} >
+                       <Image 
+                        className={styles.hero_section_image}
+                        src={classmateImage}
+                        width={500} 
+                        alt="41th batch students image with niroj sir"
+                       />
+                 </div>
+             </div>
+             {/* right_section ends*/}
+             
+
+
+          </div>
        </div>   
     </>
   )
