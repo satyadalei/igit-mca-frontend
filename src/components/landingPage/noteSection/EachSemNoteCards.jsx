2d851fabdc060f468ea7a3786811d46ed5f9f12b
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-08-20 23:36:06 +0530

    resizing syntaxes

--- src/components/landingPage/noteSection/EachSemNoteCards.jsx
+++ src/components/landingPage/noteSection/EachSemNoteCards.jsx
@@ -20,21 +20,23 @@ const EachSemNoteCards = (props) => {
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                           {props.sem_no} 
+                            {props.sem_no}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                                { props.sem_subjects && props.sem_subjects.map((subject,index)=>{
-                                    return (
-                                        <>
-                                        <span style={{display:"block"}} key={index} >{index+1}.  {subject}</span>
-                                        </>
-                                    )
-                                })}
+                            {props.sem_subjects && props.sem_subjects.map((subject, index) => {
+                                return (
+                                    <>
+                                      <span style={{ display: "block" }} key={index} >
+                                        {index + 1}.  {subject}
+                                       </span>
+                                    </>
+                                )
+                            })}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
-                    <Button style={{cursor:"pointer"}} className={styles.sem_btn} variant="contained" size="small"><DescriptionIcon/>Notes</Button>
+                    <Button style={{ cursor: "pointer" }} className={styles.sem_btn} variant="contained" size="small"><DescriptionIcon />Notes</Button>
                     <Button variant="outlined" size="small">Instructors</Button>
                 </CardActions>
             </Card>
