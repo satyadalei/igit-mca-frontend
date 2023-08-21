daf665dba82ba5dc4196f3276c062e1ced473795
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-08-21 19:28:33 +0530

    working on registration page

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -1,11 +1,17 @@
+import { Button } from '@mui/material'
 import React from 'react'
-
+import GoogleIcon from '@mui/icons-material/Google';
 const registration = () => {
   return (
     <>
       <section className='page_section' >
-         <h1>Registration page</h1>
-       </section>  
+          <div>
+                    <h1>Register via</h1>
+                    <Button variant="contained" >
+                       Google
+                    </Button>
+                </div>
+      </section>
     </>
   )
 }
