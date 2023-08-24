c6a0805657f979f8f95e2a704cdc60d38b3a3e31
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-08-25 01:23:29 +0530

    fixing-firebase-thing

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCiDlNuReV9ziIdFUeyoMXjmqiFcIiEgV4",
-  authDomain: "igit-mca-web.firebaseapp.com",
-  projectId: "igit-mca-web",
-  storageBucket: "igit-mca-web.appspot.com",
-  messagingSenderId: "1010313860858",
-  appId: "1:1010313860858:web:805905731985ad693cdb43",
-  measurementId: "G-ZHP4TJZ7D2"
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID ,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
 };
 
 // Initialize Firebase
