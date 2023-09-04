1df762b4a6c71b7854ffc1d4a58c12ebe84b650a
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-09-04 20:08:25 +0530

    login page design complete

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,96 @@
-import React from 'react'
-
-const login = () => {
+"use client"
+import React, { useState } from 'react'
+import styles from "./page.module.css"
+import { Button, TextField } from '@mui/material'
+import Image from 'next/image'
+import Link from 'next/link'
+const Login = () => {
+  const [loginDetails, setloginDetails] = useState({
+    email: "",
+    password: ""
+  });
+  const handleChange = (e) => {
+    setloginDetails((prev) => {
+      console.log(loginDetails);
+      return {
+        ...prev, [e.target.name]: e.target.value
+      }
+    })
+  }
   return (
     <>
       <section className='page_section' >
-         <h1>Login page</h1>
-       </section>  
+        <div className={styles.login_main_container} >
+
+          <div className={styles.login_box_center}>
+
+            {/* <h1 className={styles.register_via_heading} >Login</h1> */}
+
+            {/* google signin box */}
+            <div className={styles.google_signin_box} >
+              <Button style={{ backgroundColor: "#088dec", color: "white" }} className={styles.google_sign_or_login_button} >
+                <Image style={{ marginRight: "0.5rem" }} className={styles.signin_logo} src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAjVBMVEVHcEz////9/f38/Pz////4+fn9/f38/P3v8PH19vb///////87q1n8wAnrSz5HifXqOijsUUMfpUYvfvQwqVhDhvRnm/bg7ur74eDykIrznpj609DI5c/uZUj/78rG2PyzzPKJyJj5zMn/++37xSf81XhqsFCa0KV1v4f0kCGvtTCCyJyXt/hyofZDj9Yia4J9AAAAC3RSTlMAojhxnKGw/RkrfoV/rhcAAAFQSURBVDiNlVPZYoMgENQYRCIgclRNNKlpm/T+/88rl3jmofPEMuPu7LpE0X+wO8RpGh9222wW5wFxtuYntJUsaATzBSCaFV/SBhMryF9dm46xrjn5cMzh8p9KRsuypJSVTgLn/o6stKAa7Gqv9r6/kadMQ/NHX8R1u7f5Dc+aGsK6CbxPYY+dLk9rd12PnYQWz6SjbEJMW03M4Y2Qj2ZjFslg4ZUQcrZXTwMug4nUHDRPXqxAFBpKKfFsovSBwGiCYFFCGKiiEKHEYPLza/T2owpRDSaRa/MmeTXwla4j7AmFQd0kxtIrYKEK9R0G5Uy0HGPM+wrC6i41L+A4avez7kYhuYbO9ess+p8VARv0RuEgue0xB/OFaaX5GEvJsTMTFsYX0ZJ3XQP3rQ8nu7+5tLO1ztZrv3w7YM6DaIUMhCwQbDw9+z4SkKYgQdvsA/wBrYgleUhdXncAAAAASUVORK5CYII="} alt='google icon' width={30} height={30} /> Google
+              </Button>
+            </div>
+
+            <div>
+              <h3 className={styles.or_devider} >Or</h3>
+            </div>
+
+            {/* manual email password login box */}
+            <div className={styles.manual_login_box} >
+
+              {/* ---EMAIL---- */}
+              <TextField
+                style={{ margin: "0 0 0.5rem 0" }}
+                name='email'
+                required
+                fullWidth
+                // id="outlined-basic"
+                label="Email"
+                onChange={handleChange}
+                value={loginDetails.email}
+                variant="filled"
+                autoComplete='off'
+              />
+
+              {/* ---PASSWORD---- */}
+              <TextField
+                style={{ margin: "0.5rem 0 0.5rem 0" }}
+                name='password'
+                label="Password"
+                type="password"
+                required
+                fullWidth
+                // id="outlined-basic"
+                value={loginDetails.password}
+                onChange={handleChange}
+                variant="filled"
+              />
+
+              <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "0.5rem" }} >
+                <Button variant="contained" >
+                  Login
+                </Button>
+              </div>
+              <p style={{
+                marginTop: "1rem",
+                textAlign: "center"
+              }} >New user?  &nbsp;
+                <Link
+                  style={{
+                    textDecoration: "none", color: "#088dec",
+                  }}
+                  href={"/registration"} >
+                  Register now!
+                </Link> </p>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </>
   )
 }
 
-export default login
+export default Login
