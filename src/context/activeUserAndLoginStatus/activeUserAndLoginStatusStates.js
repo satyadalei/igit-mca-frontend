c5dc7c72c16009d02379e9b528ebeb76b3eac680
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-09-05 13:16:53 +0530

    fixing localstorage not found in server side

--- src/context/activeUserAndLoginStatus/activeUserAndLoginStatusStates.js
+++ src/context/activeUserAndLoginStatus/activeUserAndLoginStatusStates.js
@@ -1,34 +1,44 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import activeUserAndLoginStatusContext from "./activeUserAndLoginStatusContext"
 
 
-const ActiveUserAndLoginStatusStates = (props)=>{
+const ActiveUserAndLoginStatusStates = (props) => {
+
+    // -------- STATES ----------
     const [activeUser, setActiveUser] = useState(null);
     const [loginStatus, setLoginStatus] = useState(false);
+    const [token, setToken] = useState(null)
     
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const token = localStorage.getItem("token")
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Access localStorage here and store it in state
+            const tokenString = localStorage.getItem('token');
+            setToken(tokenString);
+        }
+    }, [])
 
     // ---------- FETCH ACTIVE USER -----------
-    const fetchActiveUser = async ()=>{
+    const fetchActiveUser = async () => {
         if (!token) {
             setLoginStatus(false)
-        }else{
+        } else {
             //fetch user & login status
             const url = `${baseUrl}/user/fetchUser`;
-            const getUser = fetch(url,{
-                method :"GET",
-                headers : {
-                    "Content-type" : "application/json",
-                    "token" : token
+            const getUser = fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-type": "application/json",
+                    "token": token
                 }
             })
             const response = await getUser.json();
             if (response.success) {
                 setLoginStatus(true);
                 setActiveUser(response.user);
-            }else{
+            } else {
                 setLoginStatus(false);
                 setActiveUser(null);
             }
@@ -36,21 +46,21 @@ const ActiveUserAndLoginStatusStates = (props)=>{
     }
 
     // -------- LOGOUT USER -----------
-    const logOutUser = ()=>{
+    const logOutUser = () => {
         localStorage.removeItem("token");
         setLoginStatus(false);
         setActiveUser(null);
     }
     return (
-      <activeUserAndLoginStatusContext.Provider value={{
-        activeUser,loginStatus, 
-        setActiveUser, 
-        setLoginStatus, 
-        fetchActiveUser,
-        logOutUser
-      }} >
-          {props.children}
-      </activeUserAndLoginStatusContext.Provider>
+        <activeUserAndLoginStatusContext.Provider value={{
+            activeUser, loginStatus,
+            setActiveUser,
+            setLoginStatus,
+            fetchActiveUser,
+            logOutUser
+        }} >
+            {props.children}
+        </activeUserAndLoginStatusContext.Provider>
     )
 }
 
