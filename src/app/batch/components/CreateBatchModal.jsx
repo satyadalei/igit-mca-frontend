0f65aabf818b9e1dacf2b0643759948ae43b6108
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-09-10 13:37:18 +0530

    validating create new batch form data

--- src/app/batch/components/CreateBatchModal.jsx
+++ src/app/batch/components/CreateBatchModal.jsx
@@ -7,176 +7,190 @@ import styles from "./Card.module.css";
 import { useState, useEffect, useContext } from "react";
 import verify from "./verifyCreateBatchFormData";
 import loadingAndAlertContext from "@/context/loadingAndAlert/loadingAndAlertContext";
-import Alert from "@/components/common/Alert";
 import Loading from "@/components/common/Loading";
+import verifyCreateBatchData from "./verifyCreateBatchFormData"
 
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "80%",
-    bgcolor: "background.paper",
-    //   border: "2px solid red",
-    boxShadow: 24,
-    p: 4,
-    maxWidth: "400px",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  bgcolor: "background.paper",
+  //   border: "2px solid red",
+  boxShadow: 24,
+  p: 4,
+  maxWidth: "400px",
 };
 
 export default function BasicModal(props) {
-    const { loading, setLoading, alert, setAlert } = useContext(
-        loadingAndAlertContext
-    );
+  const { loading, setLoading, alert, setAlert } = useContext(
+    loadingAndAlertContext
+  );
 
-    const [batch, setBatch] = useState({
-        batchNum: "",
-        strength: "",
-        startingYear: "",
-        endingYear: "",
+  const [batch, setBatch] = useState({
+    batchNum: "",
+    strength: "",
+    startingYear: "",
+    endingYear: "",
+  });
+  const handleChange = (e) => {
+    setBatch((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
     });
-    const handleChange = (e) => {
-        setBatch((prev) => {
-            return {
-                ...prev,
-                [e.target.name]: e.target.value,
-            };
-        });
-    };
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const handleCreateNewBranch = async () => {
-        //first also form verify details
-        setLoading(true)
+  };
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const handleCreateNewBranch = async () => {
+    //first also form verify details
+    const isValid = verifyCreateBatchData(batch)
+    if (isValid.error) {
+        setAlert({
+            alert: true,
+            alertType: "error",
+            alertMessage: isValid.message,
+          });
+        //   props.handleClose();
+    }else{
+        // no error call api
+        setLoading(true);
         const token = localStorage.getItem("token");
         const url = `${baseUrl}/api/batch/createNewBatch`;
         const createNewBatch = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-                token: token,
-            },
-            body: JSON.stringify(batch),
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            token: token,
+          },
+          body: JSON.stringify(batch),
         });
         const response = await createNewBatch.json();
         if (response.success) {
-            setLoading(false)
-            setAlert({
-                alert : true,
-                alertType : "success",
-                alertMessage : response.message
-            })
-            props.handleClose()
-            props.fetchAllBatch();
-            // fetch all branch & populate branch page
+          setLoading(false);
+          setAlert({
+            alert: true,
+            alertType: "success",
+            alertMessage: response.message,
+          });
+          props.handleClose();
+          props.fetchAllBatch();
+          // fetch all branch & populate branch page
         } else {
-            setLoading(false)
-            setAlert({
-                alert : true,
-                alertType : "error",
-                alertMessage : response.message
-            })
-            props.handleClose()
+          setLoading(false);
+          setAlert({
+            alert: true,
+            alertType: "error",
+            alertMessage: response.message,
+          });
+          props.handleClose();
         }
-    };
-    useEffect(() => {
-        setBatch({
-            batchNum: "",
-            strength: "",
-            startingYear: "",
-            endingYear: "",
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.open]);
+    }
+  };
+  useEffect(() => {
+    setBatch({
+      batchNum: "",
+      strength: "",
+      startingYear: "",
+      endingYear: "",
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.open]);
 
-    return (
-        <div>
-            <Modal
-                open={props.open}
-                onClose={props.handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
+  return (
+    <div>
+      <Modal
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        {loading ? (
+          <>
+            <Loading />
+          </>
+        ) : (
+          <>
+            <Box
+              sx={style}
+              style={{ display: "flex", flexDirection: "column" }}
             >
-                {loading ? (
-                    <>
-                        <Loading />
-                    </>
-                ) : (
-                    <>
-                        <Box
-                            sx={style}
-                            style={{ display: "flex", flexDirection: "column" }}
-                        >
-                            <Typography
-                                style={{ margin: "0.5rem 0 0.5rem 0", color: "#3584FC" }}
-                                id="modal-modal-title"
-                                variant="h6"
-                                component="h2"
-                            >
-                                Create a new batch
-                            </Typography>
-                            <div>
-                                <TextField
-                                    className={styles.batch_input_item}
-                                    label="Batch Number"
-                                    type="number"
-                                    placeholder="Ex : 41"
-                                    name="batchNum"
-                                    value={batch.batchNum}
-                                    onChange={handleChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                />
+              <Typography
+                style={{ margin: "0.5rem 0 0.5rem 0", color: "#3584FC" }}
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+              >
+                Create a new batch
+              </Typography>
+              <div>
+                <TextField
+                  className={styles.batch_input_item}
+                  label="Batch Number"
+                  type="number"
+                  placeholder="Ex : 41"
+                  required
+                  name="batchNum"
+                  value={batch.batchNum}
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
 
-                                <TextField
-                                    className={styles.batch_input_item}
-                                    label="Batch strength"
-                                    type="number"
-                                    placeholder="Ex : 78"
-                                    name="strength"
-                                    value={batch.strength}
-                                    onChange={handleChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                />
+                <TextField
+                  className={styles.batch_input_item}
+                  label="Batch strength"
+                  type="number"
+                  placeholder="Ex : 78"
+                  name="strength"
+                  value={batch.strength}
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
 
-                                <TextField
-                                    className={styles.batch_input_item}
-                                    label="Starting Year"
-                                    type="number"
-                                    placeholder="Ex : 2022"
-                                    name="startingYear"
-                                    value={batch.startingYear}
-                                    onChange={handleChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                />
+                <TextField
+                  className={styles.batch_input_item}
+                  label="Starting Year"
+                  type="number"
+                  placeholder="Ex : 2022"
+                  required
+                  name="startingYear"
+                  value={batch.startingYear}
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
 
-                                <TextField
-                                    className={styles.batch_input_item}
-                                    label="Ending Year"
-                                    type="number"
-                                    placeholder="Ex : 2024"
-                                    name="endingYear"
-                                    value={batch.endingYear}
-                                    onChange={handleChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                />
-                            </div>
-                            <Button
-                                onClick={handleCreateNewBranch}
-                                style={{ margin: "1rem 0 0.5rem 0", maxWidth: "200px" }}
-                                variant="contained"
-                            >
-                                Create
-                            </Button>
-                        </Box>
-                    </>
-                )}
-            </Modal>
-        </div>
-    );
+                <TextField
+                  className={styles.batch_input_item}
+                  label="Ending Year"
+                  type="number"
+                  placeholder="Ex : 2024"
+                  required
+                  name="endingYear"
+                  value={batch.endingYear}
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+              <Button
+                onClick={handleCreateNewBranch}
+                style={{ margin: "1rem 0 0.5rem 0", maxWidth: "200px" }}
+                variant="contained"
+              >
+                Create
+              </Button>
+            </Box>
+          </>
+        )}
+      </Modal>
+    </div>
+  );
 }
--- src/app/batch/components/verifyCreateBatchFormData.js
+++ src/app/batch/components/verifyCreateBatchFormData.js
@@ -1,5 +1,41 @@
-const verify = (data)=>{
+const verify = (data) => {
   console.log(data);
+
+  // check batchNum
+  if (data.batchNum === "") {
+    return {
+      error: true,
+      message: "Provide batch number"
+    }
+  }
+  // check first year
+  if (
+    data.startingYear.toString().length != 4 ||
+    data.startingYear === ""
+  ) {
+    return {
+      error: true,
+      message: "Invalid starting year"
+    }
+  }
+  // check ending year
+  if (
+    data.endingYear.toString().length != 4 ||
+    data.endingYear === ""
+  ) {
+    return {
+      error: true,
+      message: "Invalid ending year"
+    }
+  }
+
+  // everything correct
+  else{
+    return {
+      error: false,
+      message: "everything correct"
+    }
+  }
 }
 
 module.exports = verify
