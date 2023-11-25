59e744e9e2147ef86bf279b6e85592f2aa0e463f
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-11-25 16:54:21 +0530

    Fixed User Avatar & verification skeleton going towards endin mobile version

--- src/components/header/UserProfileSkeleton.jsx
+++ src/components/header/UserProfileSkeleton.jsx
@@ -6,12 +6,14 @@ const UserProfileSkeleton = ({mobileMode}) => {
         mobileMode = false ;   
     }
     return (
-        <div className={`flex ${mobileMode && 'flex-row-reverse' }`} >
+        <div className={`flex w-fit ${mobileMode && 'flex-row-reverse' }`} >
             <div className={`flex flex-col ${mobileMode ? 'items-start' : 'items-end'}  mr-1`}>
                <Skeleton variant="text"  sx={{ fontSize: '1rem', bgcolor:"white", width:"150px" }} />
                <Skeleton variant="text" sx={{ fontSize: '1rem', bgcolor:"white", width:"100px" }} />
             </div>
-            <Skeleton variant="circular" sx={{bgcolor:"white"}} width={40} height={40} />
+            <div className={`${mobileMode ? "mr-2" : "ml-2"}`} >
+               <Skeleton  variant="circular" sx={{bgcolor:"white"}} width={40} height={40} />
+            </div>
         </div>
     )
 }
